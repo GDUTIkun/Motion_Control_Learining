3cdde2a7c2e883f934546ff5db45c4dda6e89bc9
gdutikun <3961443145@qq.com>
2026-08-03 18:13:27 +0800

    clean ws

--- dynamic/2D/simulate/single_wheel_leg/controller.m
+++
@@ -1,4 +0,0 @@
-function tau = controller(x)
-%CONTROLLER Compatibility wrapper. The maintained leg controller is QP.
-tau = controller_qp(x);
-end
--- dynamic/2D/simulate/single_wheel_leg/controller_constraint.m
+++
@@ -1,4 +0,0 @@
-function tau = controller_constraint(x)
-%CONTROLLER_CONSTRAINT Compatibility wrapper. The maintained controller is QP.
-tau = controller_qp(x);
-end
